cd5ee212fb46afb8e970844f43f18eabcc4ccbb9
Manson Jones <manson.jones@gmail.com>
2021-07-15 12:45:03 -0700

    Initial Commit

---
+++ Memorize2021/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize2021
+//
+//  Created by Manson Jones on 7/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize2021/Memorize2021App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize2021App.swift
+//  Memorize2021
+//
+//  Created by Manson Jones on 7/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Memorize2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
