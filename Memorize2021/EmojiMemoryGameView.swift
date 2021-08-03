cf4d7db5bb9ae3615bb3042ced5a42f700959491
Manson Jones <manson.jones@gmail.com>
2021-08-03 11:12:50 -0700

    Add delayed animations when dealing the cards.  This takes us to Lecturer 8, 58:00.

--- Memorize2021/EmojiMemoryGameView.swift
+++ Memorize2021/EmojiMemoryGameView.swift
@@ -17,20 +17,76 @@ struct EmojiMemoryGameView: View {
     
     @ObservedObject var game: EmojiMemoryGame
     
+    @Namespace private var dealingNamespace
+    
     var body: some View {
         VStack {
             gameBody
+            deckBody
             shuffle
         }
         .padding()
     }
     
+    @State private var dealt = Set<Int>()
+    
+    private func deal(_ card: EmojiMemoryGame.Card) {
+        dealt.insert(card.id)
+    }
+    
+    private func isUndealt(_ card: EmojiMemoryGame.Card) -> Bool {
+        return !dealt.contains(card.id)
+    }
+    
+    private func dealDuration(for card: EmojiMemoryGame.Card) -> Animation {
+        var delay = 0.0
+        if let index = game.cards.firstIndex(where: { $0.id == card.id} ) {
+            delay = Double(index) * (CardConstants.totalDealDuration / Double(game.cards.count))
+        }
+        return Animation.easeInOut(duration: CardConstants.dealDuration).delay(delay)
+    }
+    
     var gameBody: some View {
         AspectVGrid(items: game.cards, aspectRatio: 2/3) { card in
-            cardView(for: card)
+            if isUndealt(card) || card.isMatched && !card.isFaceUp {
+                Color.clear
+            } else  {
+                CardView(card: card)
+                    .matchedGeometryEffect(id: card.id, in: dealingNamespace)
+                    .padding(4)
+                    .transition(
+                        AnyTransition.asymmetric(insertion: .identity, removal: .opacity))
+                    .onTapGesture {
+                        withAnimation {
+                            game.choose(card)
+                        }
+                    }
+            }
         }
-        .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
-        .padding(.horizontal)
+        .foregroundColor(CardConstants.color)
+    }
+    
+    var deckBody: some View {
+        ZStack {
+            ForEach(game.cards.filter(isUndealt)) { card in
+                CardView(card: card)
+                    .matchedGeometryEffect(id: card.id, in: dealingNamespace)
+                    .transition(AnyTransition.asymmetric(insertion: .opacity, removal: .identity))
+            }
+        }
+        .frame(
+            width: CardConstants.undealtWidth,
+            height: CardConstants.undealtHeight
+        )
+        .foregroundColor(CardConstants.color)
+        .onTapGesture {
+            //  "deal" cards
+            for card in game.cards {
+                withAnimation(dealDuration(for: card)) {
+                    deal(card)
+                }
+            }
+        }
     }
     
     var shuffle: some View {
@@ -42,16 +98,27 @@ struct EmojiMemoryGameView: View {
         }
     }
     
+    private struct CardConstants {
+        static let color = Color.red
+        static let aspectRatio: CGFloat = 2/3
+        static let dealDuration: Double = 0.5
+        static let totalDealDuration: Double = 2
+        static let undealtHeight: CGFloat = 90
+        static let undealtWidth = undealtHeight * aspectRatio
+    }
+    
     @ViewBuilder
     private func cardView(for card: EmojiMemoryGame.Card) -> some View {
-        if card.isMatched && !card.isFaceUp {
+        if isUndealt(card) || card.isMatched && !card.isFaceUp {
             Color.clear
-            // Rectangle().opacity(0)
         } else  {
             CardView(card: card)
+                .matchedGeometryEffect(id: card.id, in: dealingNamespace)
                 .padding(4)
+                .transition(
+                    AnyTransition.asymmetric(insertion: .scale, removal: .opacity))
                 .onTapGesture {
-                    withAnimation(.easeInOut(duration: 3)) {
+                    withAnimation {
                         game.choose(card)
                     }
                 }
